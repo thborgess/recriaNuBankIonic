75d7216158e96cc9be0c840b9c2ae768d14fe981
Thiago Borges <94077030+thborgess@users.noreply.github.com>
2023-02-01 22:58:08 -0300

    Commit interface nubank com ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public options: Array<any> = [
+    { icon: 'person-add-outline', text: 'Indicar amigos'},
+    { icon: 'phone-portrait-outline', text: 'Recarga de celular'},
+    { icon: 'wallet-outline', text: 'Depositar'},
+    { icon: 'options-outline', text: 'Opções'},
+    { icon: 'person-add-outline', text: 'Indicar amigos'},
+    { icon: 'help-circle-outline', text: 'Ajuda'},
+    { icon: 'barcode-outline', text: 'Pagar'},
+    { icon: 'lock-open-outline', text: 'Bloquear cartão'},
+    { icon: 'card-outline', text: 'Cartão virtual'}
+  ]
 
-  constructor() {}
+  public slideOptions: any = {slidesPerView: 3, freeMode: true}
+
+  public itens: Array<any> = [
+    { icon: 'help-circle-outline', text: 'Me ajuda'},
+    { icon: 'person-outline', text: 'Perfil'},
+    { icon: 'cash-outline', text: 'Configurar conta'},
+    { icon: 'card-outline', text: 'Configurar cartão'},
+    { icon: 'phone-portrait-outline', text: 'Configurações do app'}
+  ]
+
+  public initialStep: Number = 0;
+  private maxTranslate: Number;
+  private animation: Animation;
+
+
+  constructor(
+    private animationCtrl: AnimationController,
+    private platform: Platform;
+  ) {
+    this.maxTranslate = this.platform.height() - 200;
+  }
+
+  ngAfterViewInit() {
+    this.createAnimation()
+  }
+
+  createAnimation() {
+    this.animation = this.animationCtrl.create()
+    .addElement()
+    .duration(300)
+    .fromTo('transform', 'translateY(0)', 'translateY(${this.maxTranslate}px)')
+  }
 
 }
